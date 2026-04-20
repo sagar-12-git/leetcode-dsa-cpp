fd5780cd1de198525dddbf8b39becde07094eea2
datayogi <sagarshinde9148@gmail.com>
2026-04-20 11:05:02 +0530

    File updated at 4/20/2026, 11:05:05 AM

--- 169-MajorityElement/169-MajorityElement.cpp
+++ 169-MajorityElement/169-MajorityElement.cpp
@@ -1,19 +1,28 @@
-// Last updated: 4/20/2026, 10:52:30 AM
+// Last updated: 4/20/2026, 11:05:05 AM
 1class Solution {
 2public:
 3    int majorityElement(vector<int>& nums) {
 4        int n=nums.size();
-5        for(int val : nums){
-6            int  freq=0;
-7            for( int i: nums){
-8                if(i==val){
-9                    freq++;
-10
-11                }
-12            }if(freq>n/2){
-13                return val;
+5        sort(nums.begin(),nums.end());
+6        int freq=1;
+7        int ans=nums[0];
+8        for(int i=1;i<n;i++){
+9            if(nums[i]==nums[i-1]){
+10                freq++;
+11
+12            }else{
+13                freq=1;ans=nums[i];
 14            }
-15        }return -1;
-16    }  
-17    
-18};
+15            if (freq>n/2){
+16                return ans;
+17            }
+18
+19
+20
+21
+22
+23
+24         }return ans;
+25    }  
+26    
+27};
